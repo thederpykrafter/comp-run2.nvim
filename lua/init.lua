e27bff28cd0ca8f1057fa01f60d26100519a0f25
thederpykrafter <thederpykrafter@gmail.com>
2025-05-01 20:33:34 -0400

    formatting

--- lua/init.lua
+++ lua/init.lua
@@ -3,9 +3,14 @@ local M = {}
 function M.setup(opts)
 	opts = opts or {}
 
-	vim.keymap.set("n", opts.keymaps.runDefault or "<leader>RR", function()
-		if true then print("Hello") end
-	end, { desc = "Hello" })
+	vim.keymap.set(
+		"n",
+		opts.keymaps.runDefault or "<leader>RR",
+		function()
+			if true then print("Hello") end
+		end,
+		{ desc = "Hello" }
+	)
 end
 
 return M
